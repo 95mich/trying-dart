43891c8425f45b51a7ccdfc47ef2d69fd0e5a684
95mich <67961477+95mich@users.noreply.github.com>
2021-07-06 21:13:19 +0700

    Create card_deck.dart

---
+++ card_deck.dart
@@ -0,0 +1,20 @@
+class Deck{
+  List<Card> cards;
+  
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+    
+    for (var suit in suits){
+      for (var rank in ranks){
+        
+      }
+    }
+  }
+}
+
+class Card{
+  String suit;
+  String rank;
+}
