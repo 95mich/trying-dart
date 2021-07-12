f88cfba8fa7e322af92ac41139d7be4a0b8862d1
95mich <67961477+95mich@users.noreply.github.com>
2021-07-13 05:07:40 +0700

    Update bloc_getter.dart

--- bloc_getter.dart
+++ bloc_getter.dart
@@ -12,8 +12,13 @@ void main() {
 
 class Bloc {
   final emailController = StreamController<String>();
+  final passwordController = StreamController<String>();
   
   //GETTER TO MAKE IT EASIER TO CALL
   get changeEmail => emailController.sink.add;
   get email => emailController.stream;
+  
+  //or as function that accept string
+  Function(String) get changePassword => passwordController.sink.add;
+  Function(String) get password => passwordController.stream;
 }
